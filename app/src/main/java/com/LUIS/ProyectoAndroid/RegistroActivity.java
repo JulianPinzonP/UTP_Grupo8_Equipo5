c2104832707641c3f9ed7f6c4b4137d475aece93
LuisAngelB01 <angelmeneses013@gmail.com>
2021-11-11 12:49:26 -0500

    Interfaz de registro 2.0

--- app/src/main/java/com/LUIS/ProyectoAndroid/RegistroActivity.java
+++ app/src/main/java/com/LUIS/ProyectoAndroid/RegistroActivity.java
@@ -3,12 +3,26 @@ package com.LUIS.ProyectoAndroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_registro);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
